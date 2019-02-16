cc4a05766e2c2b8faff0e3ccac51b63f3831b62a
Giorgi Khmaladze <gkhmalad@purdue.edu>
2019-02-15 21:40:30 -0500

    Added database querying code and updated comments.

--- Conley's Test Website/signin.php
+++ Conley's Test Website/signin.php
@@ -37,7 +37,20 @@
     }
     else{
 
-        $hashed_password = ""; //! This needs to be fetched from the database
+        //? Link the database (Values subject to change)
+        $link = mysqli_connect("localhost","users","password","users");
+
+        //? Generate the query command/code
+        $query = "SELECT password FROM users WHERE `email`= ".$email;
+
+        //? Query the database
+        $result = msqli_query($link, $query);
+
+        //? Get the row from database as an array
+        $row = mysqli_fetch_array($result);
+
+        //? Single out the hashed password from the row array
+        $hashed_password = $row['password'];
 
         if(password_verify($password, $hashed_password)) {
             // If the password inputs matched the hashed password in the database
