904b5d963be9f57461bff6d5589db1ce8fce363e
Mihir Somani <imihirsomani@gmail.com>
2019-02-20 03:30:28 -0500

    Sucessful login now redirects to market page

--- Conley's Test Website/signin.php
+++ Conley's Test Website/signin.php
@@ -39,18 +39,12 @@
         }else{
             //echo "Database connection successful!";
         }
-        //? Generate the query command/code
         $query = "SELECT password FROM users WHERE `email` = '".$email."'";
-        //? Query the database
         $result = mysqli_query($link, $query);
-        //? Get the row from database as an array
         $row = mysqli_fetch_array($result);
-        //? Single out the hashed password from the row array
         $hashed_password = $row['password'];
         if(password_verify($password, $hashed_password)) {
-            // If the password inputs matched the hashed password in the database
-            // Log them in.
-            echo "Login sucess!";
+            header("Location: market.php");
         }else{
             echo "Login failure!";
         } 
