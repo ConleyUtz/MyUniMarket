38316e821c5495c6af1d16eb69a2523a043e1173
Mihir Somani <imihirsomani@gmail.com>
2019-02-20 22:19:18 -0500

    Added items table

--- EnvironmentSetup/my_uni_market.sql
+++ EnvironmentSetup/my_uni_market.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 21, 2019 at 03:56 AM
+-- Generation Time: Feb 21, 2019 at 04:18 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.1
 
@@ -25,11 +25,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `itemId` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `price` decimal(11,2) NOT NULL,
+  `location` text NOT NULL,
+  `category` int(11) NOT NULL,
+  `quality` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `userId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`itemId`, `name`, `price`, `location`, `category`, `quality`, `description`, `userId`) VALUES
+(1, 'Item1', '50.99', 'Hawkins Hall', 2, 2, 'This is the description text.', 1),
+(2, 'Item2', '99.12', 'PMU', 3, 1, 'This is the description text.', 2);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `userId` int(10) UNSIGNED NOT NULL,
   `email` text NOT NULL,
   `username` text NOT NULL,
   `password` text NOT NULL
@@ -39,7 +64,7 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `username`, `password`) VALUES
+INSERT INTO `users` (`userId`, `email`, `username`, `password`) VALUES
 (1, 'user1@purdue.edu', 'user1', '$2y$10$6YDgveHu1FjX5Xek81h6Ue4b1GMGQFTrbHdZCUNAGKBAusJ45x2sq'),
 (2, 'user2@purdue.edu', 'user2', '$2y$10$zwXYhSqEZQqbyfO/3dqFR.KcDoSxdNRMMsV3OP4zWfG.RPLXxROBi');
 
@@ -48,20 +73,32 @@ INSERT INTO `users` (`id`, `email`, `username`, `password`) VALUES
 --
 
 --
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`itemId`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`userId`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `itemId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `userId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
