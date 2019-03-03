30362b0bd2db7345de9b4ad409e40bac0f37f6db
Giorgi Khmaladze <gkhmalad@purdue.edu>
2019-03-02 21:50:11 -0500

    Created a snipped for field validator

---
+++ PHP Snippets/fieldValidator.php
@@ -0,0 +1,33 @@
+<?php
+  if ($_POST){
+
+  //! Checking if field 1 has correct input in it
+  if(!$_POST['<<field1Name>>']){
+
+    $error .= "Error message";
+
+  }
+  else{
+
+    //! If field has value DO SOMETHING
+  }
+
+  //! Checking if field 2 has correct input in it
+  if(!$_POST['<<field2Name>></field2Name>']){
+
+    $error .= "Error message";
+  }
+  else{
+
+      //! If field has value DO SOMETHING
+  }
+
+  else{
+
+      /**
+       * IFF all fields have correct values in them execute the following code.
+       */
+  }
+
+  }
+?>
