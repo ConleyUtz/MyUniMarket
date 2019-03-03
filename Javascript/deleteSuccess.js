040c99bbd8cb1dbb5bce888c382c6135bf5713dd
Sakshic663 <40264238+Sakshic663@users.noreply.github.com>
2019-03-03 17:32:38 -0500

    Correct redirection path

--- Javascript/deleteSuccess.js
+++ Javascript/deleteSuccess.js
@@ -3,7 +3,7 @@ function deleteSuccess(deleteButtonID) {
     document.getElementById(deleteButtonID).onclick = function(){    
         var r = confirm("Do you really want to delete your account?"); 
         if (r == true) {
-            window.location.href="try2.html";
+            window.location.href="signup.php";
             alert("Account deleted");
           } 
     }
