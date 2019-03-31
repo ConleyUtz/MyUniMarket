af296020f415af57418907a40c57bacb6ad2da3d
Mihir Somani <imihirsomani@gmail.com>
2019-03-31 18:55:45 -0400

    Removed Testing folder

--- Testing/Sakshi/logoutSuccess.js
+++
@@ -1,9 +0,0 @@
-function logoutSuccess(logoutButtonID) {
-    
-    document.getElementById(logoutButtonID).onclick = function(){    
-        window.location.href="signin.php";
-        alert("You have been successfully logged out!");
-
-    }
-
-}
