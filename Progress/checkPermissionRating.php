fedf40a4adfcf958d7321824db65c525ad997f7d
Giorgi Khmaladze <gkhmalad@purdue.edu>
2019-04-06 22:51:30 -0400

    Implemented validation for rating

---
+++ Progress/checkPermissionRating.php
@@ -0,0 +1,53 @@
+<?php
+
+  //? DB Connection
+  include 'DatabaseConnection.php';
+
+  //? Session start && variable
+  session_start();
+  $testerID = "";
+  $permissionArr = "";
+  $checkEmail = false;
+
+  //! userID of the person whose profile is being viewed
+  $ratedID = "";
+
+  //? Check session
+  if(!$_SESSION['email']){
+
+    header('Location: signin.php'); 
+  }
+  else{
+
+    $testerID = $_SESSION['email'];
+  }
+
+  //? Database Connect
+  $dbConnection = DatabaseConnection::getInstance()->getConnection();
+
+  //TODO HAS TO CHANGE FOR PROFILE PAGES
+  $query = "SELECT ratePermission FROM users WHERE `userId` = ".$ratedID;
+
+  if($result = mysqli_query($dbConnection, $query)){
+
+    $row = mysqli_fetch_array($result);
+
+    if(!empty($row['ratePermission']))
+      $permissionArr = explode(',' , $row['ratePermission']);
+  }
+
+  for($i=0, $i < count($permissionArr), $i++){
+
+    if($permissionArr[$i] == $_SESSION['email']){
+
+      $checkEmail = true;
+      break;
+    }
+  }
+
+  if($checkEmail){
+
+    //TODO Implement ability to rate
+  }
+
+?>
