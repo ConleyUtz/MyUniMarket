2c81933f41e333706fa914179897c684f8ed61e8
Mihir Somani <imihirsomani@gmail.com>
2019-04-07 03:24:08 -0400

    Changed account2.php to account-settings.php

--- WebApplication/account.php
+++ WebApplication/account.php
@@ -137,7 +137,7 @@
                             <ul class="vertical menu">
                                 <li><a href="account.php">My Listings</a></li>
                                 <li><a href="404.php">Bookmarked Items</a></li>
-                                <li><a href="account2.php">Account Settings</a></li>
+                                <li><a href="account-settings.php">Account Settings</a></li>
                             </ul>
                         </div><!-- widget content /-->
                     </div><!-- widget /-->
