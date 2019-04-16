91a16e706df5eea0e60a508188e5dcef2d13916f
gkhmalad <gkhmalad@purdue.edu>
2019-04-16 02:08:22 -0400

    Reverted Heroku Testing Changes

--- WebApplication/DatabaseConnection.php
+++ WebApplication/DatabaseConnection.php
@@ -5,10 +5,10 @@ class DatabaseConnection{
     private static $instance = null;
     private $connection;
 
-    private $host = 'us-cdbr-iron-east-02.cleardb.net';
-    private $username = 'b754fe3ec25f9a';
-    private $password = 'e3908fef';
-    private $database = 'heroku_3789075718b803c';
+    private $host = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $database = 'my_uni_market';
 
     private function __construct(){
         $this->connection = mysqli_connect($this->host, $this->username, $this->password, $this->database);
